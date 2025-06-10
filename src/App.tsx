01808287a0da7c77c68b6e8b56376c744823a626
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-06-10 12:24:12 +0530

    Add: App.tsx which has basic info about my self

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  console.log("Hii");
+  
+  return (
+    <>
+      <h1>Hello React</h1>
+      <h2>Name: Nikhil Kiritbhai Prajapati</h2>
+      <p>Hii,my name is Nikhil and I am  React traniee at simform</p>
+      <a href="mailto:nikhil.prajapati@simform.dom">Email: nikhil.prajapati@simformsolutions.com</a>
+      <p>
+        Github ID:
+        <a href="https://github.com/Nikhil9723">https://github.com/Nikhil9723</a>
+      </p>
+      
+    </>
+  );
+}
+
+export default App;
